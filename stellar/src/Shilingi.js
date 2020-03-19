e6bf5d82850f54193b6db0e2bef16b3749d74f50
BrianMwangi21 <mwangibrian8@gmail.com>
2020-03-19 10:31:13 +0300

    Cached shilingi.js

--- stellar/src/Shilingi.js
+++
@@ -1,7 +0,0 @@
-import StellarSdk from 'stellar-sdk';
-
-// Creates SHILINGI asset issued by account from secret
-const source = StellarSdk.Keypair.fromSecret('SAP467V4Q5MQGTDKOLJS2EBPDMIQECUNI632N2VTOBPZ2V76K4D2TCG7');
-var shilingiAsset = new StellarSdk.Asset('KES4042', source.publicKey());
-
-export default shilingiAsset;
